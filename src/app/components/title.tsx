28b4e481654d2c919e06fabbb9209ef911415626
kirill pavlov <2020kirillp@gmail.com>
2024-08-01 10:08:32 -0700

    Smoothed out animation with framer motion

--- src/app/components/title.tsx
+++ src/app/components/title.tsx
@@ -6,6 +6,7 @@ export default function Title() {
 				className="mx-auto mt-10 md:mt-24 flex flex-col items-start text-center p-5"
 			>
 				<h1 className="text-primary-color text-7xl">Kirill Pavlov</h1>
+
 				<div className="flex flex-col items-start text-left w-full">
 					<p className="text-xs text-custom-accent">37.7749° N, 122.4194° W</p>
 					<p className="text-xs text-custom-accent">San Francisco, CA</p>
--- src/app/components/work.tsx
+++ src/app/components/work.tsx
@@ -1,29 +1,89 @@
 "use client";
 import Link from "next/link";
 import projectsData from "../../../public/projects.json";
-import { useState, useRef, useEffect } from "react";
+import { useState } from "react";
+import { motion, useMotionValue, useSpring } from "framer-motion";
+
+const MotionLink = motion(Link);
+const linkVariants = {
+	initial: { x: 0 },
+	hover: { x: -20, color: "#ff4921" },
+};
+const categoryVariants = {
+	initial: { x: 0 },
+	hover: { x: 20, color: "#ff4921" },
+};
+
+const CustomCursor = ({ mousePosition }) => {
+	const springConfig = { damping: 40, stiffness: 900 };
+	const x = useSpring(mousePosition.x, springConfig);
+	const y = useSpring(mousePosition.y, springConfig);
+	return (
+		<motion.div
+			className="fixed p-4 text-xs top-0 left-0 w-16 h-16 rounded-full bg-custom-accent flex items-center justify-center text-primary-color pointer-events-none z-50 uppercase"
+			style={{
+				left: x,
+				top: y,
+				translateX: "-50%",
+				translateY: "-50%",
+			}}
+		>
+			View
+		</motion.div>
+	);
+};
 
 export default function Work() {
 	const projects = Object.values(projectsData.projects).slice(0, 3);
+	const [hoveredLink, setHoveredLink] = useState(null);
+	const mouseX = useMotionValue(0);
+	const mouseY = useMotionValue(0);
+
+	const springConfig = { damping: 60, stiffness: 900 };
+	const mouseXSpring = useSpring(mouseX, springConfig);
+	const mouseYSpring = useSpring(mouseY, springConfig);
+
+	const handleMouseMovement = (e) => {
+		mouseX.set(e.clientX);
+		mouseY.set(e.clientY);
+	};
 
 	return (
 		<div id="work" className="container mt-10">
+			{hoveredLink !== null && (
+				<CustomCursor mousePosition={{ x: mouseX, y: mouseY }} />
+			)}
 			<div className="mx-auto flex flex-col relative">
-				{Object.entries(projects).map(([key, project]) => (
-					<Link
+				<h3 className="text-primary-color text-center mb-6">Recent Projects</h3>
+
+				{Object.entries(projects).map(([key, project], index) => (
+					<MotionLink
 						key={key}
 						href={`/work/${encodeURIComponent(
 							project.name.toLowerCase().replace(/ /g, "-")
 						)}`}
 						className="flex flex-row justify-between border-t border-b border-primary-color p-10 items-center group hover:cursor-none"
+						whileHover="hover"
+						initial="initial"
+						onMouseMove={handleMouseMovement}
+						onMouseEnter={() => setHoveredLink(index)}
+						onMouseLeave={() => setHoveredLink(null)}
 					>
-						<p className="text-primary-color text-3xl mt-1 group-hover:-translate-x-3 group-hover:text-custom-accent transform duration-200">
+						<motion.p
+							className="text-primary-color text-3xl mt-1"
+							variants={linkVariants}
+							transition={{ duration: 0.2 }}
+						>
 							{project.name.toUpperCase()}
-						</p>
-						<p className="hidden md:block text-primary-color text-xs group-hover:translate-x-3 group-hover:text-custom-accent transform duration-200">
+						</motion.p>
+						<motion.p
+							className="hidden md:block text-primary-color text-xs"
+							variants={categoryVariants}
+							transition={{ duration: 0.2 }}
+						>
 							{project.category}
-						</p>
-					</Link>
+						</motion.p>
+					</MotionLink>
 				))}
 
 				<a
