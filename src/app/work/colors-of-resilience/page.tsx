41f4ecfa936a8cbfb75b7e245ae456d734b4f87b
kirill pavlov <2020kirillp@gmail.com>
2024-08-02 10:10:46 -0700

    Fix typos and update project description

--- src/app/work/colors-of-resilience/page.tsx
+++ src/app/work/colors-of-resilience/page.tsx
@@ -32,9 +32,9 @@ const ColorsOfResilience = () => {
 			</div>
 
 			<p className="text-primary-color text-sm mt-6">
-				This is an online art marketplace. I udes figma to create the design for
+				This is an online art marketplace. I used figma to create the design for
 				the page. I used Next.js with Typescript and Tailwind to style and
-				animate the page. For the backend I used PostgreSQL satabase and Prisma
+				animate the page. For the backend I used PostgreSQL database and Prisma
 				as an ORM. I have designed and developed an online art marketplace using
 				a modern tech stack. The user interface was meticulously crafted using
 				Figma, ensuring a visually appealing and intuitive user experience. The
@@ -43,9 +43,9 @@ const ColorsOfResilience = () => {
 				Tailwind CSS was employed to efficiently style the components and create
 				smooth, engaging animations throughout the page. On the backend, I
 				implemented a PostgreSQL database to securely store and manage the
-				application's data. To streamline database operations and ensure
+				applications data. To streamline database operations and ensure
 				efficient querying, I utilized Prisma as an Object-Relational Mapping
-				(ORM) tool. Prisma's type-safe database client and intuitive API
+				(ORM) tool. Prismas type-safe database client and intuitive API
 				significantly enhanced the development process and maintainability of
 				the codebase.
 			</p>
